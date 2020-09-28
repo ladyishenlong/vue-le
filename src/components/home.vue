68908183bda0cfa009bfcafef8839a7c63d6068d
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2020-09-28 10:21:05 +0800

    vue学习

---
+++ src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>这是首页</h1>
+
+    <el-button @click="goto">这是个按钮</el-button>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods:{
+    goto(){
+      this.$router.push({path:"/user"})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    这是用户界面
+
+    <el-button @click="request">网络请求</el-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user",
+
+  methods:{
+    request(){
+
+      this.$axios.get('/test').then((result)=>{
+        console.log(result);
+      })
+
+
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
