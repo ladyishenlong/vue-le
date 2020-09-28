59efba55df3e11a927b29c4ffcc69e7170ce9020
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2020-09-28 10:12:20 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import VueRouter from 'vue-router';
+
+import home from '@/components/home'
+import user from "@/components/user";
+
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL='http://localhost:8992/';
+
+
+
+
+
+
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueRouter);
+
+//路由跳转路径
+let router = new VueRouter({
+    routes: [
+        {path: '/home', component: home},
+        {path: '/user', component: user}
+    ]
+});
 
 new Vue({
-  render: h => h(App),
+    el: '#app',
+    router: router,
+    render: h => h(App),
 }).$mount('#app')
